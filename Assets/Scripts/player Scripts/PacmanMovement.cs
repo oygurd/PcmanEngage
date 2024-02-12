20df879f22db0a54bdea3e106f5e46bf761d50ea
oygurd <86951294+oygurd@users.noreply.github.com>
2024-02-13 00:48:05 +0200

    Making a movement script + raycasts for player
    using raycast to see which walls are hit and which not, will be used for more precise movement of the player, need to be used firstly for testings.

--- Assets/Scripts/player Scripts/PacmanMovement.cs
+++ Assets/Scripts/player Scripts/PacmanMovement.cs
@@ -4,15 +4,72 @@ using UnityEngine;
 
 public class PacmanMovement : MonoBehaviour
 {
+    [SerializeField] Rigidbody PacmanRb;
+    [SerializeField] float Speed;
+
+    [SerializeField] float rayDistance;
+
+
+    //*//
+    public Vector3 forward;
+    public Vector3 backward;
+    public Vector3 right;
+    public Vector3 left;
+    //*// 
+
+    [SerializeField] bool rayHit;
+    [SerializeField] LayerMask CheckWallLayer;
+    RaycastHit hit; //get information back from a raycast
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rayDistance = 2.1f;
+        PacmanRb = GetComponent<Rigidbody>();
+
+        //*//       
+        forward = transform.TransformDirection(Vector3.forward);
+        backward = transform.TransformDirection(Vector3.back);
+        right = transform.TransformDirection(Vector3.right);
+        left = transform.TransformDirection(Vector3.left);
+        //*//
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        DrawVectorRay();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, rayDistance, CheckWallLayer))
+        {
+            rayHit = true;
+        }
+        else
+        {
+            rayHit = false;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawCube(transform.position, Vector3.forward);
+    }
+
+    public void DrawVectorRay()//using debug command to see the distance of all rays to have a visual confirmation
+    {
+        Debug.DrawRay(transform.position, forward * rayDistance, Color.blue);
+
+        Debug.DrawRay(transform.position, backward * rayDistance, Color.cyan);
+
+        Debug.DrawRay(transform.position, right * rayDistance, Color.red);
+
+        Debug.DrawRay(transform.position, left * rayDistance, Color.cyan);
+
+
     }
 }
