7636c3076fa84885aeaa209d07b5a001cce3c474
lavi <lavinaorab@gmail.com>
2024-03-17 16:07:36 +0200

    fix problems git is hard

---
+++ Assets/Scripts/ghosts/afraid_mode_changer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class afraid_mode_changer : MonoBehaviour
+{
+    [SerializeField] ghost_input ghost_Input;
+    [SerializeField] int active;
+    [SerializeField] bool afraid;
+
+    private IEnumerator afraidFor(float time)
+    {
+        active++;
+        ghost_Input.arraymod2 = 2;
+        afraid = true;
+        yield return new WaitForSeconds(time);
+        active--;
+        if (active == 0)
+        {
+            ghost_Input.arraymod2 = -1;
+            afraid = false;
+        }
+    }
+}
