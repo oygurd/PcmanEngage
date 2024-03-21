7c305bc4623913939444b2618a39fb0fbe0c8927
oygurd <86951294+oygurd@users.noreply.github.com>
2024-03-21 23:51:25 +0200

    more

--- Assets/Scripts/powers/ActivateLightning.cs
+++ Assets/Scripts/powers/ActivateLightning.cs
@@ -6,11 +6,20 @@ using UnityEngine.VFX;
 public class ActivateLightning : MonoBehaviour
 {
     [SerializeField] VisualEffect lightning;
+    public bool pressed = false;
 
 
+    private void Update()
+    {
+        playLightningBolt();
+    }
     public void playLightningBolt()
     {
-        //lightning.Play();
-        lightning.SendEvent("onPlayBolt");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            lightning.SendEvent("onPlayBolt");
+            pressed = true;
+        }
+
     }
 }
