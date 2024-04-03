a22fe953e304bb7a107d3551ac5c4b65641dc7eb
oygurd <86951294+oygurd@users.noreply.github.com>
2024-04-04 00:41:36 +0300

    Global volume in lightning - not finished
    בטעות שכחתי לסדר את הכל ועשיתי שינויים על לביא, אסדר בהמשך.

--- Assets/Scripts/powers/ActivateLightning.cs
+++ Assets/Scripts/powers/ActivateLightning.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.VFX;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class ActivateLightning : MonoBehaviour
 {
+    [SerializeField] Animator distortion; //activates global volume to add more effects
     [SerializeField] VisualEffect lightning;
     public bool pressed = false;
 
@@ -13,13 +16,19 @@ public class ActivateLightning : MonoBehaviour
     {
         playLightningBolt();
     }
+
     public void playLightningBolt()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             lightning.SendEvent("onPlayBolt");
             pressed = true;
+            distortion.SetBool("Distortion", true);
+
         }
+        
 
     }
+
+        
 }
--- Assets/Scripts/powers/checForLightning.cs
+++ Assets/Scripts/powers/checForLightning.cs
@@ -9,6 +9,8 @@ public class checForLightning : MonoBehaviour
     [SerializeField] float efectDistens, timeOfEfect;
     [SerializeField] ghost_controler1 controler;
     [SerializeField] VisualEffect lightning;
+    [SerializeField] Animator distortion; //activates global volume to add more effects
+   
 
     public void startChec()
     {
@@ -16,8 +18,14 @@ public class checForLightning : MonoBehaviour
         Vector3 myPosishen = new Vector3(this.transform.position.x, 0, this.transform.position.z);
         if (Vector3.Distance(myPosishen, player) <= efectDistens)
         {
+            distortion.SetBool("Distortion", true);
             lightning.SendEvent("onPlayBolt");
             StartCoroutine(inRech(timeOfEfect));
+        }
+        else
+        {
+            distortion.SetBool("Distortion", false);
+
         }
     }
     private IEnumerator inRech(float time)
@@ -27,10 +35,10 @@ public class checForLightning : MonoBehaviour
         controler.stand = false;
     }
 
-/*    private void OnDrawGizmosSelected()
-    {
-        Vector3 myPosishen = new Vector3(this.transform.position.x, 0, this.transform.position.z);
-        Gizmos.color = Color.blue;
-        Gizmos.DrawSphere(myPosishen, efectDistens);
-    }*/
+    /*    private void OnDrawGizmosSelected()
+        {
+            Vector3 myPosishen = new Vector3(this.transform.position.x, 0, this.transform.position.z);
+            Gizmos.color = Color.blue;
+            Gizmos.DrawSphere(myPosishen, efectDistens);
+        }*/
 }
