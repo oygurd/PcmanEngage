f7309976f3331d7a59b8c6d389fb22e9ef0a1bb1
lavi <lavinaorab@gmail.com>
2024-04-07 16:34:11 +0300

    death done

--- Assets/Scripts/interactables/manager.cs
+++ Assets/Scripts/interactables/manager.cs
@@ -8,6 +8,8 @@ public class manager : MonoBehaviour
     [SerializeField] float helf;
     [SerializeField] float points;
     [SerializeField] TextMeshProUGUI textPoint, textHelf;
+    [SerializeField] Vector3 playerStart, ghostStart;
+    [SerializeField] GameObject player, red, blue, oreng, pink;
 
     public void AddPoints(float newPoints)
     {
@@ -27,9 +29,30 @@ public class manager : MonoBehaviour
         textPoint.text = points.ToString();
     }
 
+    public void playerDeath()
+    {
+        Debug.Log("why");
+        if (helf > 0)
+        {
+            helf--;
+            textHelf.text = helf.ToString();
+            player.transform.position = playerStart;
+            red.transform.position = ghostStart;
+            blue.transform.position = ghostStart;
+            oreng.transform.position = ghostStart;
+            pink.transform.position = ghostStart;
+        }
+        else
+        {
+            Debug.Log("loos");
+        }
+    }
+
+
+
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
