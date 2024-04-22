254ed6723ec1ec244d4377df598779361e377cf9
lavi <lavinaorab@gmail.com>
2024-04-22 13:41:50 +0300

    ice power litel fix

--- Assets/Scripts/powers/SelfDestruct.cs
+++ Assets/Scripts/powers/SelfDestruct.cs
@@ -4,12 +4,17 @@ using UnityEngine.Events;
 public class SelfDestruct : MonoBehaviour
 {
     [SerializeField] float selfDestructDelay, meltTime; // Time before the object self-destructs
-    [SerializeField] UnityEvent melt;
+    [SerializeField] UnityEvent melt, spown;
 
     void Start()
     {
+        Invoke("spownAnimashen", 0);
         Invoke("meltAnimashen", selfDestructDelay - meltTime );
         Invoke("DestroyPrefab", selfDestructDelay);
+    }
+    void spownAnimashen()
+    {
+        spown.Invoke();
     }
 
     void meltAnimashen()
